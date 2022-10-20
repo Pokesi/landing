ad2330778a9933e52902823cc60c56cc72e6e6ff
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-10-20 20:57:21 +1100

    Add IPFS support

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ const languages = [
 ]
 
 module.exports = {
+  pathPrefix: '__GATSBY_IPFS_PATH_PREFIX__',
   plugins: [
     {
       // including a plugin from outside the plugins folder needs the path to it
@@ -42,6 +43,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-emotion`,
+    'gatsby-plugin-ipfs',
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
